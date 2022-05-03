392f5419ad058275996d1a88a06bd472ad68732d
houdae <houdaelmaaroufi@hotmail.fr>
2022-05-03 15:23:50 +0200

    btn click check

--- app/src/App/components/ui/Button/Button.js
+++ app/src/App/components/ui/Button/Button.js
@@ -1,23 +1,31 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import style from './Button.module.css'
 import propTypes from 'prop-types'
-const Button = (props)=>{
-    console.log(props)
-    return (<button
-    className={style.Button} 
-    style={{...props.style, backgroundColor: PushSubscriptionOptions.bgColor, color : props.color}}
-    type={props.type}
-    onClick={(evt)=>{
-        props.onButtonClicked();
 
-    }}
-    
-    >
-
-    {props.children}
-    </button>)
-    
-}
+const Button = (props) =>{
+    const [isClicked, setIsClicked] = useState(false);
+    useEffect(() => {
+      console.log('isClicked a changer ->',isClicked)
+      if(true===isClicked){
+          setTimeout(()=>{setIsClicked(false)},1000)
+      }
+    }, [isClicked])
+    return (
+      <button
+        className={style.Button}
+        style={{...props.style, backgroundColor: props.bgColor, color: props.color }}
+        type={props.type}
+        onClick={(evt)=>{
+          setIsClicked(true);
+          props.onButtonClicked();
+        }}
+      >
+        {props.children}
+        <br/>
+        {isClicked.toString()}
+      </button>
+    );
+  };
 
 Button.propTypes={
     children: propTypes.any.isRequired,
